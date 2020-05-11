c8c3fe7585273e47dc1291462db9449aecefc8e4
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-05-11 15:17:41 -0400

    Button links to login page

--- client/src/components/loginButton.js
+++ client/src/components/loginButton.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import {Link} from "react-router-dom";
 
 function LoginButton(){
 	return(
-		<Button>
-			Log in to save
-		</Button>
+		<Link to = "/login">
+			<Button>
+				Log in to save builds
+			</Button>
+		</Link >
 	)
 }
 export default LoginButton;
