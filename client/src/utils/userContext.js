3fc01391395b73bfff74a0af32858b1e50ef5f49
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-05-13 19:21:16 -0400

    Made user context

---
+++ client/src/utils/userContext.js
@@ -0,0 +1,20 @@
+import React, { useReducer, useContext, createContext } from "react";
+
+const UserContext = createContext();
+const { Provider } = UserContext;
+const reducer = (state, action) => {
+	return { authenticated: true, userName: action.userName }
+}
+
+// Stores the currently selected armor for users
+const UserProvider = ({ value = [], ...props }) => {
+	const [state, dispatch] = useReducer(reducer, {
+		authenticated: false,
+		userName: ""
+	});
+	return <Provider value={[state, dispatch]}{...props} />
+}
+const useUserContext = () => {
+	return useContext(UserContext);
+};
+export { UserProvider, useUserContext };
