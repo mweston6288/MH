d4207a4ee2873b1027e77b197f7c44469bcf4f16
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-05-19 13:28:55 -0400

    Added a logout option and relocated build and login/logout links to the right of the page.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/monsters", {
 });
 
 //if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 //}
 
 // routes go here
